7ca745e73c76200b87295f698a06611d94b3e3c2
LDH9276 <leedh9276@naver.com>
2023-09-17 20:17:08 +0900

    비회원 게시판 오픈 버그 수정

--- src/Board/VisitedModule.jsx
+++ src/Board/VisitedModule.jsx
@@ -1,11 +1,12 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 import "swiper/css";
 import "swiper/css/free-mode";
 import Subscribe from "./Subscribe";
 
-function VisitedModule({ boardVisited = [], userSubscribe = [], handleSubscribe}) {
+function VisitedModule({ boardVisited = [], userSubscribe = [], handleSubscribe }) {
+    const userId = useSelector((state) => state.userId);
     const [toggle, setToggle] = useState(false);
 
     return (
@@ -20,28 +21,38 @@ function VisitedModule({ boardVisited = [], userSubscribe = [], handleSubscribe}
             ) : (
                 <div className="board-visitlist-wrap">
                     <div className="board-visitlist-legend">방문 게시판</div>
-                        <ul className="board-visitlist">
-                            {boardVisited.slice(0,4).map((board) => (
-                                <li key={board.id}>
-                                    <Link to={`/board/${board.id}`} className="board-visitlist-item">
-                                        {board.board_name}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
+                    <ul className="board-visitlist">
+                        {boardVisited.slice(0, 4).map((board) => (
+                            <li key={board.id}>
+                                <Link to={`/board/${board.id}`} className="board-visitlist-item">
+                                    {board.board_name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
                     <div className="board-visitlist-subscribe">
-                        <button onClick={() => {toggle ? setToggle(false) : setToggle(true)}}>
-                            <img src={`${process.env.PUBLIC_URL}/btn/list-arrow.svg`} alt="구독한 게시판" className={!toggle ? 'board-visitlist-icon' : 'board-visitlist-icon active'} />
+                        <button
+                            onClick={() => { toggle ? setToggle(false) : setToggle(true); }} >
+                            <img src={`${process.env.PUBLIC_URL}/btn/list-arrow.svg`} alt="구독한 게시판" className={!toggle ? "board-visitlist-icon" : "board-visitlist-icon active"} />
                         </button>
                     </div>
-                    <div className={!toggle ? 'board-subscribe-list' : 'board-subscribe-list active'}>
-                        <ul>
-                            <li>구독한 게시판</li>
-                            {userSubscribe.map((board) => (
-                                <Subscribe key={board.board_id} boardId={board.board_id} board={board.board_name} handleSubscribe={handleSubscribe}/>
-                            ))}
-                        </ul>
-                    </div>
+
+                    {userId ? (
+                        <div className={!toggle ? "board-subscribe-list" : "board-subscribe-list active"}>
+                            <ul>
+                                <li>구독한 게시판</li>
+                                {userSubscribe.map((board) => (
+                                    <Subscribe key={board.board_id} boardId={board.board_id} board={board.board_name} handleSubscribe={handleSubscribe} />
+                                ))}
+                            </ul>
+                        </div>
+                    ) : (
+                        <div className={!toggle ? "board-subscribe-list" : "board-subscribe-list active"}>
+                            <ul>
+                                <li>로그인 후 구독한 게시판을 확인할 수 있습니다.</li>
+                            </ul>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
